efbee7ffe4e553edaecdf26892b9bce98e0a1cc5
Aligern <sasso.luca93@gmail.com>
2024-06-10 18:23:29 +0200

    print API on console

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@
 </template>
 
 <script>
+import {store} from './store.js'
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      store,
+      projects: []
+    }
+  },
+  methods: {
+    getAllProjects(){
+      axios.get(this.store.apiBaseUrl + '/projects').then((res) => {
+        console.log(res.data);
+      });
+    }
+  },
+  mounted() {
+    this.getAllProjects();
+  }
 }
 </script>
 
