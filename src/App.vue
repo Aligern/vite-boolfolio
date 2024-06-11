4dbc1cd24fe64d02a99148d7bac49dabd662066d
Aligern <sasso.luca93@gmail.com>
2024-06-11 19:31:41 +0200

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
+    <HeaderComponent />
     <h1>Boolfolio</h1>
-
-
-    <ul>
-      <li v-for="(project, index) in menuProjects" :key="index">
-        <router-link :to="{ name: project.routeName }" class="nav-link">
-            {{ project.label }} 
-        </router-link>
-      </li>
-    </ul>
-
     <main class="container">
       <router-view></router-view>
     </main>
-
 </template>
 
 <script>
+import { store } from './store.js';
+import HeaderComponent from './components/HeaderComponent.vue';
 export default {
   name: 'App',
+  components: {
+    HeaderComponent
+  },
   data() {
     return {
-      menuProjects: [
-        {
-          label: 'Home',
-          routeName: 'home',
-        },
-        {
-          label: 'Projects',
-          routeName: 'projects',
-        },
-      ]
+      store,
     }
   }
 }
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-
-    </div>
+    <nav class="nav d-flex justify-content-between">
+            <div>
+                <h1>logo</h1>
+            </div>
+            <div>
+                <button v-for="(project, index) in menuProjects" :key="index">
+                    <router-link :to="{ name: project.routeName }" class="nav-link btn">
+                        {{ project.label }} 
+                    </router-link>
+                </button>
+            </div>
+    </nav>
 </template>
 
 <script>
     export default {
-        
+        name: 'HeaderComponent',
+        data() {
+            return {
+            menuProjects: [
+                {
+                label: 'Home',
+                routeName: 'home',
+                },
+                {
+                label: 'Projects',
+                routeName: 'projects',
+            },
+        ]
     }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
